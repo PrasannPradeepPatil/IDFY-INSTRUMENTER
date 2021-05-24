6009db593e35304ea99c6007f18f8627e2f5b484
prasann <prasannpatil98@gmail.com>
2021-05-24 11:43:03 +0530

    idfyInstrumenterChanges

--- idfyInstrumenter.py
+++ idfyInstrumenter.py
@@ -1,6 +1,44 @@
 """
-takes datetime map  type string ; returns 
+  Log function that takes level as argument
+
+  ## Parameters
+   - level: 'info', 'warn', 'error'
+   - raw_event: map containing the actual event
+   - opts:
+     - log (bool): Log to stdout (default `true`)
+     - publish (bool): Publish log to event bus (default `false`)
+
+  ## Examples
+
+      log(level, raw_event, opts)
+      "Success"
+
+      log(level, raw_event, opts)
+      [log: "Some error", publish: "Some error"]
 
 """
+levels = ["info","warn","error"]
+def log(level, raw_event, opts= []):
+    if(level in levels):
+        print(level, raw_event, opts, dir())
 
 
+"""
+  Info log
+"""
+def info(raw_event, opts=[]):
+    print("info", raw_event, opts, dir())
+
+
+"""
+  Warning log
+"""
+def warn(raw_event, opts=[]):
+    print("warn", raw_event, opts, dir())
+
+
+"""
+  Error log
+"""
+def error(raw_event, opts=[]):
+    print("error", raw_event, opts, dir())
