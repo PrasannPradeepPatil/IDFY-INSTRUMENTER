275568fcb9addeab913c1adbabdff10296bd6cdc
prasann <prasannpatil98@gmail.com>
2021-06-08 11:49:56 +0530

    packageChanges

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-   name='Instrumenter',
+   name='IDFYINSTRUMENTER',
    version='1.0',
    description='Idfy Instrumenter that takes logs and publishes them to RABBITMQ as well as the console',
    author='Prasann Patil',
    author_email='prasann@flairlabs.com',
-   packages=['Instrumenter'],  #same as name
-   install_requires=['pika', 'sys', 'json','os','datetime','threading','uuid'], #external packages as dependencies
+   packages=['IDFYINSTRUMENTER'],  #same as name
+   install_requires=['pika'], #external packages as dependencies
 )
 
